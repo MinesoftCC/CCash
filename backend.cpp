5f3247b7a7144301c51e6fabdb38a2c808644a0b
William Katz <qualitycontentquestionmark@gmail.com>
2020-12-28 07:28:40 +0000

    :tada:

---
+++ backend.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+#include <drogon/HttpClient.h>
+#include "BankAPI.h"
+
+int main(int argc, char *argv[])
+{
+        if (argc != 2)
+        {
+                std::cout << "Usage: sudo ./CCash <bank password>\n";
+                return 0;
+        }
+        if (geteuid() != 0)
+        {
+                std::cout << "ERROR: CCash MUST be ran as root\n";
+                return 0;
+        }
+        std::cout << "process started, PID: " << getpid() << "\n\n";
+        auto primary_bank = std::make_shared<BankAPI>(argv[1]);
+        app().setLogPath("./").setLogLevel(trantor::Logger::kDebug).addListener("0.0.0.0", 80).registerController(primary_bank);
+        //app().enableRunAsDaemon();
+        auto handlerInfo = app().getHandlersInfo();
+        for (auto &info : handlerInfo)
+        {
+                std::cout << std::get<0>(info);
+                switch (std::get<1>(info))
+                {
+                case Get:
+                        std::cout << " (GET) ";
+                        break;
+                case Post:
+                        std::cout << " (POST) ";
+                        break;
+                case Delete:
+                        std::cout << " (DELETE) ";
+                        break;
+                case Put:
+                        std::cout << " (PUT) ";
+                        break;
+                case Options:
+                        std::cout << " (OPTIONS) ";
+                        break;
+                case Head:
+                        std::cout << " (HEAD) ";
+                        break;
+                case Patch:
+                        std::cout << " (PATCH) ";
+                        break;
+                default:
+                        break;
+                }
+                std::cout << '\n';
+        }
+        app().run();
+
+        return 0;
+}
+
+//TODO: precompiled binaries
